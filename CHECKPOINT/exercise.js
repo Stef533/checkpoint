9c803e10b7cf2d22323da32af352c9ff71835ae1
Stef533 <137123241+Stef533@users.noreply.github.com>
2023-09-20 12:04:44 +0200

    Add files via upload

---
+++ CHECKPOINT/exercise.js
@@ -0,0 +1,8 @@
+//Partendo da un array di stringhe
+//const words = ['spray', 'limit', 'elite', 'exuberant', 'present'];
+//trasforma ogni lettera in MAIUSCOLO. Stampa in console il risultato.
+
+const words = ['spray', 'limit', 'elite', 'exuberant', 'present'];
+const terzaParola = words[2];
+
+console.log(terzaParola.toUpperCase());
